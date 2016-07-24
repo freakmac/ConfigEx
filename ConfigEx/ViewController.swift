8ea7cda54dad4bd56b977d7646122c7edf3d5e23
Charles J Cromer <cjc4@alumni.duke.edu>
2016-07-23 21:52:43 -0400

    Initial Commit

---
+++ ConfigEx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConfigEx
+//
+//  Created by Jeremy Cromer on 7/23/16.
+//  Copyright © 2016 Jeremy Cromer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
